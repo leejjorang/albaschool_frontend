6cdba09a7b3dac9e2f4e80e32d9a30cddbce644e
Lee-chaerin <5543705@naver.com>
2025-01-10 17:28:30 +0900

    Feat: 가게 등록(알바) 페이지 구조 및 스타일 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import styled from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import Layout from "./components/layout/Layout";
+import RegisterStore from "./pages/staff/RegisterStore";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/common/Home";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
       <Layout>
-        "test"
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store/register/staff" element={<RegisterStore />} />
+        </Routes>
       </Layout>
-    </>
+    </BrowserRouter>
   )
 }
 
