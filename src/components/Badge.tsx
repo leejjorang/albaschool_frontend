64d9fd3d2a857aabe4a2c8ab020d23dd20044f3d
Lee-chaerin <5543705@naver.com>
2025-01-14 05:32:15 +0900

    Feat: 채팅 목록(공통) 페이지 컴포넌트화

---
+++ src/components/Badge.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+interface BadgeProps {
+  message: string | number;
+}
+
+const Badge: React.FC<BadgeProps> = ({message}) => {
+  return (
+    <BadgeStyle>
+      {message}
+    </BadgeStyle>
+  )
+}
+
+export default Badge;
+
+
+const BadgeStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #FF9D3C;
+  color: white;
+  border-radius: 50%;
+  width: 1.3rem;
+  height: 1.3rem;
+`
---
+++ src/components/chat/ChatListBox.tsx
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import Badge from "../Badge";
+import { useNavigate } from "react-router-dom";
+
+interface ChatListBoxProps {
+  id: number;
+  storeName: string;
+  headCount: number;
+  lastMessage: string;
+  time: string;
+  badge?: number;
+}
+
+const ChatListBox: React.FC<ChatListBoxProps> = ({id, storeName, headCount, lastMessage, time, badge}) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/chats/${id}`);
+  }
+
+  return (
+    <ChatListBoxStyle onClick={handleClick}>
+      <div style={{maxWidth: '60%'}}>
+        <span>
+          <p>{storeName}</p>
+          <p style={{color: '#7E7E7E'}}>{headCount}</p>
+        </span>
+        <p style={{color: '#565656'}}>{lastMessage}</p>
+      </div>
+      <TimeBadgeBoxStyle>
+        <p>{time}</p>
+        {badge && <Badge message={badge} />}
+      </TimeBadgeBoxStyle>
+    </ChatListBoxStyle>
+  )
+}
+
+export default ChatListBox;
+
+
+const ChatListBoxStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid #CDCDCD;
+  width: 93%;
+  padding: 0.7rem 1rem;
+  border-radius: 10px;
+
+  span {
+    display: inline-flex;
+    gap: 0.5rem;
+    font-size: 1.4rem;
+    margin-bottom: 0.3rem;
+  }
+
+  p {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  &:focus, &:hover {
+    background-color: #FFD400;
+  }
+`
+
+const TimeBadgeBoxStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-between;
+`
