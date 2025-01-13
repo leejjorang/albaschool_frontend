52ea2dc6584e06c1b1fa5b2515b45b43ea29cb14
Lee-chaerin <5543705@naver.com>
2025-01-13 11:10:45 +0900

    Feat: 유저 수정(공통) 페이지 구조 및 스타일 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import StaffRegisterStore from "./pages/staff/RegisterStore";
 import ManagerRegisterStore from "./pages/manager/RegisterStore";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/common/Home";
-import User from "./pages/staff/User";
+import StaffUser from "./pages/staff/User";
+import ManagerUser from "./pages/manager/User";
+import UserEdit from "./pages/common/UserEdit";
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/store/register/staff" element={<StaffRegisterStore />} />
           <Route path="/store/register/manager" element={<ManagerRegisterStore />} />
-          <Route path="/user" element={<User />} />
+          <Route path="/user/staff" element={<StaffUser />} />
+          <Route path="/user/manager" element={<ManagerUser />} />
+          <Route path="/user/edit" element={<UserEdit />} />
         </Routes>
       </Layout>
     </BrowserRouter>
