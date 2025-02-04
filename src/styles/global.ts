31c8be00ade1369eb883b29caf847daab6db3b99
Lee-chaerin <5543705@naver.com>
2025-02-04 22:51:41 +0900

    Design: 폰트 변경

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,37 +8,48 @@ export const GlobalStyle = createGlobalStyle`
     font-style: normal;
   }
   @font-face {
-    font-family: 'MangoDdobak-B';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2405-3@1.1/MangoDdobak-B.woff2') format('woff2');
-    font-weight: 700;
-    font-style: normal;
-  }
-  @font-face {
     font-family: 'GmarketSansMedium';
     src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
     font-weight: normal;
     font-style: normal;
   }
   @font-face {
-    font-family: 'SUIT-Regular';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
-    font-weight: normal;
+    font-family: 'Paperlogy-8ExtraBold';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-8ExtraBold.woff2') format('woff2');
+    font-weight: 800;
     font-style: normal;
   }
+  @font-face {
+    font-family: 'LINESeedKR-Bd';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+  }
+  @font-face {
+     font-family: 'S-CoreDream-3Light';
+     src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
+     font-weight: normal;
+     font-style: normal;
+  }    
 
   * {
     padding: 0;
     margin: 0;
     overflow-x: hidden;
     box-sizing: border-box;
-    font-family: 'GmarketSansMedium';
+    font-family: 'LINESeedKR-Bd';
   }
 
   h1 {
     font-family: 'HakgyoansimDunggeunmisoTTF-B';
   } 
+
+  h2, h3, h4, h5, h6 {
+    font-family: 'GmarketSansMedium';
+  }
   
   p {
-    font-family: 'SUIT-Regular';
+    font-family: 'S-CoreDream-3Light';
+    font-weight: bold;
   }
 `
