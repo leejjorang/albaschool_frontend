ff535af5b6b9ae126221bc1ac4bce5bdb24c3a47
Lee-chaerin <5543705@naver.com>
2025-02-04 22:55:57 +0900

    Feat: BigButton 컴포넌트 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,6 +15,14 @@ export const Button: React.FC<ButtonProps> = ({type, message, width, onClick}) =
   )
 }
 
+export const BigButton: React.FC<ButtonProps> = ({type, message, width, onClick}) => {
+  return (
+    <BigButtonStyle type={type} style={{width: `${width}%`}} onClick={onClick} >
+      {message}
+    </BigButtonStyle>
+  )
+}
+
 export const NegativeButton: React.FC<ButtonProps> = ({message, width, onClick}) => {
   return (
     <NagativeButtonStyle style={{width: `${width}%`}} onClick={onClick}>
@@ -29,6 +37,20 @@ const ButtonStyle = styled.button`
   border: 1px solid #CDCDCD;
   border-radius: 15px;
   padding: 0.7rem 1.1rem;
+  text-align: center;
+  font-size: 1.2rem;
+  cursor: pointer;
+
+  &:focus, &:hover {
+    background-color: #FFD400;
+  }
+` 
+
+const BigButtonStyle = styled.button`
+  background-color: #FAED7D;
+  border: 1px solid #CDCDCD;
+  border-radius: 15px;
+  padding: 0.9rem 1rem;
   text-align: center;
   font-size: 1.2rem;
   cursor: pointer;
